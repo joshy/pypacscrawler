70c9498f84f2f11b078d74a95fd64a643f2769e3
Joshy Cyriac <j.cyriac@gmail.com>
2017-02-17 15:23:07 +0100

    added first test

---
+++ tests/test_dicom.py
@@ -0,0 +1,19 @@
+import unittest
+import dicom
+
+
+class DicomTest(unittest.TestCase):
+    def test_start(self):
+        line = 'W:'
+        start = dicom._is_start_or_end(line)
+        self.assertEqual(True, start)
+
+    def test_tag(self):
+        line = 'W: (0008,0060) CS [MR] #   2, 1 Modality'
+        tag = dicom._get_tag(line)
+        self.assertEqual('Modality', tag)
+
+    def test_value(self):
+        line = 'W: (0008,0060) CS [MR] #   2, 1 Modality'
+        tag = dicom._get_value(line)
+        self.assertEqual('MR', tag)
