40985be59819adc4a97b69cfe8dcd169bc39f4be
Joshy Cyriac <j.cyriac@gmail.com>
2017-07-05 18:28:06 +0200

    json and csv files are generated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## v0.0.3 - 05.06.2017
+ * csv and json files are produced
+
 ## v0.0.2 - 18.04.2017
  * Get rid of pacs size limit restriction
--- pypacscrawler/writer.py
+++ pypacscrawler/writer.py
@@ -10,9 +10,9 @@ def _get_file_name(month: str, day: str, mod: str):
         os.makedirs(OUTPUT_DIR)
     file_name = os.path.join(OUTPUT_DIR, 'data-')
     if month:
-        return file_name + month + '.json'
+        return file_name + month
     else:
-        return file_name + day + '-' + mod + '.json'
+        return file_name + day + '-' + mod
 
 
 def write_results(results, month, day, mod):
@@ -20,4 +20,5 @@ def write_results(results, month, day, mod):
     file_name = _get_file_name(month, day, mod)
     frames = pd.concat([pd.DataFrame(x) for x in results if len(x) > 0])
     logging.info('Writing results to file %s', file_name)
-    frames.to_json(file_name, orient='records')
+    frames.to_csv(file_name + '.csv', index=False, sep=';')
+    frames.to_json(file_name+ '.json', orient='records')
