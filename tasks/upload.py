48e8a1be3e65794c64bfc74b79172c79dbc71037
Joshy Cyriac <j.cyriac@gmail.com>
2017-11-09 17:44:49 +0100

    new luigi task for uploading month data

---
+++ tasks/upload.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+import luigi
+import requests
+
+from tasks.month import MonthTask
+
+
+class UploadTask(luigi.Task):
+    url = luigi.Parameter()
+    month = luigi.Parameter()
+
+    def requires(self):
+        return MonthTask(month=self.month)
+
+    def run(self):
+        logging.debug('Uploading to url %s', self.url)
+        headers = {'content-type': 'application/json'}
+        params = {'commit': 'true'}
+        payload = self.input().open('rb').read()
+        r = requests.post(self.url, data=payload, params=params, headers=headers)
+        if r.status_code == requests.codes.ok:
+            with self.output().open('w') as outfile:
+                outfile.write('DONE')
+
+    def output(self):
+        month_file = os.path.basename(self.input().path)
+        return luigi.LocalTarget('data/%s.uploaded' % month_file)
+
+
+if __name__ == '__main__':
+    luigi.run()
